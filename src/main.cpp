fca3a632547c32c2676bd6cf76d985623974a089
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2022-05-03 22:56:29 -0700

    add note to change to tank

--- src/main.cpp
+++ src/main.cpp
@@ -253,6 +253,8 @@ void autonomous() {
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+// note: change to tank
 [[noreturn]] void opcontrol() {
     //imu.tare();
 
